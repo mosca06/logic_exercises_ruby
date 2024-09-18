0715603160320eda6005a21e49f142f06a1f2d3e
Eduardo Mello <eduardomell06@hotmail.com>
2024-09-18 20:59:27 -0300

    feat: imc

--- exercises/imc.rb
+++ exercises/imc.rb
@@ -1,7 +1,32 @@
 class Imc
-  def self.calculate(height,weight)
-    #your code here \/ \/ \/
+  @imc_ranges = [
+    { threshold: 18.6, description: 'Abaixo do peso' },
+    { threshold: 24.9, description: 'peso normal' },
+    { threshold: 29.9, description: 'Sobrepeso' },
+    { threshold: 34.9, description: 'Obesidade grau 1' },
+    { threshold: 39.9, description: 'Obesidade grau 2' }
+  ]
 
-    #your code here /\ /\ /\
+  def self.calculate(height,weight)
+
+    height_in_meters = parse_height(height)
+
+    imc = (weight / (height_in_meters * height_in_meters)).round(2)
+    
+    rate_imc(imc)
+    
   end
+
+  def self.parse_height(height)
+    height / 100.0
+  end
+
+  def self.rate_imc(imc)
+    # Encontra a faixa correta usando a chave do hash
+    @imc_ranges.each do |range|
+      return "#{imc} - #{range[:description]}" if imc <= range[:threshold]
+    end
+    "#{imc} - Obesidade grau 3"
+  end
+
 end
