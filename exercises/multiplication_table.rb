b8e9b4eaf645480e8258eb530f01c9f38d73deca
Eduardo Mello <eduardomell06@hotmail.com>
2024-09-19 09:11:15 -0300

    feat: multiplication_table

--- exercises/multiplication_table.rb
+++ exercises/multiplication_table.rb
@@ -1,7 +1,26 @@
 class MultiplicationTable
+  @@index = 1
+  @@result = ''
+  
   def self.calculate(number)
-    #your code here \/ \/ \/
+    @@result = ''
+    @@index = 1
+    loop_through(number)
+  end
 
-    #your code here /\ /\ /\
+  def self.loop_through(number)
+    while @@index <= 10
+      @@result += write_rule(number)
+      @@index += 1
+    end
+    @@result
+  end
+
+  def self.write_rule(number)
+    @@index == 10 ? "#{number}x#{10} = #{number * 10}" : "#{number}x#{@@index} = #{operation(number)}, "
+  end
+
+  def self.operation(number)
+    number * @@index
   end
 end
