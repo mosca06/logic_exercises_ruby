0b0137e92b9d679e0fbaf3ac0ef505588eccf02c
Eduardo <eduardomell06@hotmail.com>
2024-09-21 21:52:59 -0300

    odd even

--- exercises/even_or_odd.rb
+++ exercises/even_or_odd.rb
@@ -1,5 +1,20 @@
 class EvenOrOdd
   def self.calculate(number)
-    number % 2 > 0 ? "#{number} is odd" : "#{number} is even"
+    isDecimal(number) ? "#{number} is #{isOddEven(getDecimal(number))}" : "#{number} is #{isOddEven(number)}"
   end
+
+  def self.isDecimal(number)
+    number.class == Float
+  end
+
+  def self.getDecimal(number)
+    number.to_s.split('.').last.to_i
+  end
+
+
+  def self.isOddEven(number)
+    puts number
+    number % 2 > 0 ? "odd" : "even"
+  end 
+
 end
