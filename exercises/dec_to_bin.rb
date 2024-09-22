92f1c79f3098cc496520afcec955d020cf572027
Eduardo <eduardomell06@hotmail.com>
2024-09-22 20:07:09 -0300

    feat:DectoBin

--- exercises/dec_to_bin.rb
+++ exercises/dec_to_bin.rb
@@ -1,7 +1,21 @@
 class DecToBin
   def self.convert(number)
-    #your code here \/ \/ \/
+    binary_number_to_invert = []
+    number_to_divide = number
 
-    #your code here /\ /\ /\
+    while number_to_divide > 0
+      binary_number_to_invert << divide_by_two_rest(number_to_divide)
+      number_to_divide = divide_by_two(number_to_divide)
+    end
+
+    binary_number_to_invert.reverse.join.to_i
+  end
+
+  def self.divide_by_two(number)
+    number / 2
+  end
+
+  def self.divide_by_two_rest(number)
+    number % 2
   end
 end
