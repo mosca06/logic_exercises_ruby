190603b089bbf88fd0b3f57ab3eab2c2d8775f75
Eduardo <eduardomell06@hotmail.com>
2024-09-22 10:30:53 -0300

    compund interest

--- exercises/compound_interest.rb
+++ exercises/compound_interest.rb
@@ -1,7 +1,9 @@
 class CompoundInterest
   def self.calculate(initial_amount, fee, months)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    compoundAmount = initial_amount
+    months.times do
+      compoundAmount += compoundAmount * fee/100.0
+    end
+    "R$ #{compoundAmount.round(2)}"
   end
 end
