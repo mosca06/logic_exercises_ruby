a0d30fce37f38d9333cceb79ebaad80e9f7fc49d
Eduardo <eduardomell06@hotmail.com>
2024-09-29 08:24:34 -0300

    feat:fibonacci

--- exercises/fibonacci.rb
+++ exercises/fibonacci.rb
@@ -1,7 +1,16 @@
 class Fibonacci
   def self.calculate(number)
-    #your code here \/ \/ \/
+    @@fibonacci_results= [
+      0,
+      1
+    ]
 
-    #your code here /\ /\ /\
+    return @@fibonacci_results[number] unless @@fibonacci_results[number].nil?
+
+    2.upto(number) do |num|
+      @@fibonacci_results[num] =  @@fibonacci_results[num - 1] + @@fibonacci_results[ num - 2]
+    end
+
+    @@fibonacci_results[number]
   end
 end
