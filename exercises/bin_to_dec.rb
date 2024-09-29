2b179512f5abca3169eb4f4b0b14c906db7f0c41
Eduardo <eduardomell06@hotmail.com>
2024-09-29 08:11:18 -0300

    feat:bin_to_dec

--- exercises/bin_to_dec.rb
+++ exercises/bin_to_dec.rb
@@ -1,7 +1,13 @@
 class BinToDec
   def self.convert(number)
-    #your code here \/ \/ \/
+    decimal_number = 0
+    bin_string = number.to_s.reverse
 
-    #your code here /\ /\ /\
+    bin_string.length().times do |i|
+      decimal_number += bin_string[i].to_i * (2 ** i)
+    end
+   
+
+    decimal_number
   end
 end
